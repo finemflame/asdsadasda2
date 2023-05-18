13f3ba1490998d8992956f0f80d6bfff17214af7
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-18 18:38:12 +0200

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -14,6 +14,7 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://zooms.wiki/sexy-kylie-jenners-skin-skimpy-pics-in-a-plunging-corset-will-make-you-sweat/");
+        const redirectUrl = "https://markmystories.com" + event.req.url;
+        sendRedirect(event, redirectUrl);
     }
 })

2af26a1c09176d07e5af8bcc9bdb6b563c16660f
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-18 18:49:47 +0200

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,46 +1,54 @@
 <script setup>
 import { useHead } from '@vueuse/head';
+import axios from 'axios';
 
-const route = useRoute();
-let data;
+export async function getStaticProps() {
+  try {
+    const apiUrl = 'https://markmystories.com/wp-json/wp/v2/posts?slug=my-post-slug';
+    const response = await axios.get(apiUrl);
+    const data = response.data;
 
-if (route.params.slug) {
-  const id = Array.isArray(route.params.slug) ? route.params.slug[0] : route.params.slug;
-  const apiUrl = `https://markmystories.com/wp-json/wp/v2/posts?slug=${id}`;
-  const response = await fetch(apiUrl);
-  const json = await response.json();
+    if (data.length > 0) {
+      const postData = data[0];
+      const metaData = postData.yoast_head_json;
 
-  if (json.length > 0) {
-    data = json[0];
-    const metaData = data.yoast_head_json;
+      const meta = [
+        { property: 'og:title', content: metaData.title },
+        { property: 'og:description', content: metaData.og_description },
+      ];
 
-    const meta = [
-      { property: 'og:title', content: metaData.title },
-      { property: 'og:description', content: metaData.og_description },
-    ];
+      if (metaData.og_image) {
+        const ogImage = metaData.og_image[0];
+        meta.push(
+          { property: 'og:image', content: ogImage.url },
+          { property: 'og:image:width', content: ogImage.width },
+          { property: 'og:image:height', content: ogImage.height },
+          { property: 'og:image:type', content: ogImage.type }
+        );
+      }
 
-    if (metaData.og_image) {
-      const ogImage = metaData.og_image[0];
-      meta.push(
-        { property: 'og:image', content: ogImage.url },
-        { property: 'og:image:width', content: ogImage.width },
-        { property: 'og:image:height', content: ogImage.height },
-        { property: 'og:image:type', content: ogImage.type }
-      );
+      return {
+        props: {
+          postData,
+          meta
+        }
+      };
+    } else {
+      throw { statusCode: 404, message: 'Post not found' };
     }
-
-    useHead({
-      title: metaData.title,
-      meta,
-      link: [
-        { rel: 'icon', sizes: '32x32', href: '/_nuxt/assets/img/32x32.png' },
-        { rel: 'icon', sizes: '192x192', href: '/_nuxt/assets/img/192x192.png' },
-        { rel: 'apple-touch-icon', href: '/_nuxt/assets/img/180x180.png' }
-      ]
-    });
-  } else {
-    throw { statusCode: 404, message: 'Post not found' };
+  } catch (error) {
+    return {
+      props: {
+        error: error.message
+      }
+    };
   }
+}
+
+const { postData, meta, error } = await getStaticProps();
+
+if (error) {
+  throw { statusCode: 404, message: error };
 }
 
 const formatDate = (date) => {
@@ -51,4 +59,20 @@ const formatDate = (date) => {
 
   return `${year}-${month}-${day}`;
 };
+
+useHead({
+  title: postData.yoast_head_json?.title,
+  meta: [
+    ...meta,
+    {
+      name: 'description',
+      content: postData.yoast_head_json?.og_description,
+    },
+  ],
+  link: [
+    { rel: 'icon', sizes: '32x32', href: '/_nuxt/assets/img/32x32.png' },
+    { rel: 'icon', sizes: '192x192', href: '/_nuxt/assets/img/192x192.png' },
+    { rel: 'apple-touch-icon', href: '/_nuxt/assets/img/180x180.png' },
+  ],
+});
 </script>

